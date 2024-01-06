c03ab0ceb8841b014dc538a90ce74a7555c2f247
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2024-01-06 18:21:21 +0100

    Add icons, restyle book

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgIconsModule } from '@ng-icons/core';
+import { featherHome } from '@ng-icons/feather-icons';
 import { BreadcrumbsComponent } from '@shared/components/breadcrumbs/breadcrumbs.component';
 import { DividerPipe } from '@shared/pipes/divider/divider.pipe';
 
@@ -11,7 +13,13 @@ import { DividerPipe } from '@shared/pipes/divider/divider.pipe';
     // Pipes
     DividerPipe,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgIconsModule.withIcons({
+      featherHome,
+    }), // NOTE: see all available icons at https://feathericons.com/],
+  ],
   exports: [
     // Components
     BreadcrumbsComponent,
