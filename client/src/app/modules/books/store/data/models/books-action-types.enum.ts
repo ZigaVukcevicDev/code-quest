8ff73ab46816f652ec95e5523b41b8a902a7defe
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2024-01-06 12:36:59 +0100

    Refactor books action types

---
+++ client/src/app/modules/books/store/data/models/books-action-types.enum.ts
@@ -0,0 +1,5 @@
+export enum BooksActionTypes {
+  LOAD_BOOKS_DATA = '[Books] Load books data',
+  LOAD_BOOKS_SUCCESS = '[Books] Load books success',
+  LOAD_BOOKS_ERROR = '[Books] Load books error',
+}
