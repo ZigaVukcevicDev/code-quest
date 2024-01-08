ce1516fda74a9bd3bd74ff57ccec237183f76d4d
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2024-01-08 21:08:15 +0100

    Update reducer for list favorite

--- client/src/app/modules/books/store/effects/book-list-favorite.effect.ts
+++ client/src/app/modules/books/store/effects/book-list-favorite.effect.ts
@@ -1,34 +1,67 @@
 import { Injectable } from '@angular/core';
 import { BooksService } from '@app/data/services/api/books/books.service';
+import { BooksFavoriteService } from '@app/data/services/books-favorite/books-favorite.service';
 import {
   loadBookListFavoriteAction,
   loadBookListFavoriteErrorAction,
   loadBookListFavoriteSuccessAction,
 } from '@app/modules/books/store/actions/book-list-favorite.action';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, exhaustMap, map } from 'rxjs/operators';
+import { forkJoin, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class BookListFavoriteEffects {
+  /**
+   * Note to code reviewer:
+   *
+   * I decided to put the logic (booksService and booksFavoriteService method calls)
+   * into following reducer. This way those two services stay independent.
+   *
+   * The other solution would be to use booksService inside of booksFavoriteService,
+   * which would reduce logic in reducer but services would not be anymore independent.
+   * I don't know what would be better, discussion desired.
+   */
+
   loadBookListFavorite$ = createEffect(() =>
     this.actions$.pipe(
       ofType(loadBookListFavoriteAction),
-      // TODO
-      exhaustMap(() =>
-        this.booksService.getBookList().pipe(
-          map((bookList) => {
-            console.log('effect loadBookListFavorite$ success', bookList);
-            return loadBookListFavoriteSuccessAction({ payload: bookList });
-          }),
-          catchError((error) => {
-            console.log('effect loadBookListFavorite$ error', error);
-            return of(loadBookListFavoriteErrorAction());
+      switchMap(() =>
+        this.booksFavoriteService.getFavoriteBookListIds().pipe(
+          switchMap((favoriteBooks) => {
+            // TODO: refactor this part with better naming and comment rxjs operators
+            // Perform HTTP calls using bookService
+            const httpRequests = favoriteBooks.map((favoriteBook) =>
+              this.booksService.getBookById(favoriteBook.bookId).pipe(
+                map((book) => ({
+                  id: book.id, // TODO
+                  name: book.name,
+                  authors: book.authors,
+                  publisher: book.publisher,
+                  isFavorite: true, // TODO: really? or do i even need this for favorites?
+                }))
+              )
+            );
+
+            return forkJoin(httpRequests).pipe(
+              map((bookList) => {
+                console.log('effect loadBookListFavorite$ success', bookList);
+                return loadBookListFavoriteSuccessAction({ payload: bookList });
+              }),
+              catchError((error) => {
+                console.log('effect loadBookListFavorite$ error', error);
+                return of(loadBookListFavoriteErrorAction());
+              })
+            );
           })
         )
       )
     )
   );
 
-  constructor(private actions$: Actions, private booksService: BooksService) {}
+  constructor(
+    private actions$: Actions,
+    private booksService: BooksService,
+    private booksFavoriteService: BooksFavoriteService
+  ) {}
 }
