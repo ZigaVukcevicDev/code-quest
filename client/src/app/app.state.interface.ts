ab438ed93a4cef433640f3a093ac6b8adf693f1d
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2024-01-08 20:53:05 +0100

    Update state interface

--- client/src/app/app.state.interface.ts
+++ client/src/app/app.state.interface.ts
@@ -1,5 +1,7 @@
+import { BookListFavoriteState } from '@app/modules/books/store/data/models/book-list-favorite.state.interface';
 import { BookListState } from '@app/modules/books/store/data/models/book-list.state.interface';
 
 export interface AppState {
   bookList: BookListState;
+  bookListFavorite: BookListFavoriteState;
 }
